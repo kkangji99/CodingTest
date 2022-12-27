0350ac788bfb766633c4a95ff73114b381d71624
kkangji99 <81802175+kkangji99@users.noreply.github.com>
2022-12-27 23:46:19 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 65.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int price) {
+        if(price>=500000)
+        {
+            price = (int)(price-(price*0.2));
+        }
+        else if(price>=300000)
+        {
+            price = (int)(price-(price*0.1));
+        }
+        else if(price>=100000)
+        {
+            price = (int)(price-(price*0.05));
+        }
+        return price;
+    }
+}
