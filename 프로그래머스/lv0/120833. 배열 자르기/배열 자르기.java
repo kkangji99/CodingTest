29934add92b72bc0ffc4622a5764f73cd725e272
kkangji99 <81802175+kkangji99@users.noreply.github.com>
2022-12-27 23:47:03 +0900

    [level 0] Title: 배열 자르기, Time: 0.02 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int a=0;
+        int n = num2-num1+1;
+        int[] answer = new int[n];
+        
+         for(int i = 0; i<=numbers.length; i++)
+        {
+            if(i>=num1&&i<=num2)
+                answer[a++] = numbers[i];
+        }
+
+        return answer;
+    }
+}
