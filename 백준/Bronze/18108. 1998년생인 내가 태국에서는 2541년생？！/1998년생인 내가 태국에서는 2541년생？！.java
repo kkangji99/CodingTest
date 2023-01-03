50e0345adadaba86da8ef51ab5df7868ff47e00f
kkangji99 <81802175+kkangji99@users.noreply.github.com>
2023-01-04 00:51:13 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,9 @@
+//불기를 서기로 바꿀 때 543를 빼주면 된다.
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        System.out.println(year-543);
+    }
+}
